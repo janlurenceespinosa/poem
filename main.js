7d7542b0a0dd2f2a13748dddf1aab6a7cec0a772
janlurenceespinosa <144762683+janlurenceespinosa@users.noreply.github.com>
2023-09-13 18:53:55 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+const main = document.querySelector('.main');
+const cancel = document.querySelector('.cancel');
+const msg = document.querySelector('#msg');
+
+while (true) {
+        alert('I’m waiting for you, day by day.');
+        alert('In the library, I swear I’ll stay,');
+        alert('Waiting for your unforgettable smile,');
+        alert('To see you, even just for a while.');
+        alert('Following you where you go,');
+        alert('Although I’m tired and being slow.');
+        alert('You’re the only one who can give me joy,');
+        alert('You’re my greatest dream, My Library Girl');
+        main.style.display = 'block';
+        break;
+    
+}
+
+
+
+
+
+
+
